aa87f0451eace50d5ed61f90a585ab2a22aa82a5
Esan Alorizi <62729091+EsanOrizi@users.noreply.github.com>
2023-03-19 21:24:31 +0000

    Character Service Added

--- DotNet7/Controllers/CharacterController.cs
+++ DotNet7/Controllers/CharacterController.cs
@@ -1,4 +1,5 @@
 ﻿using DotNet7.Models;
+using DotNet7.Services.CharacterService;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.OpenApi.Writers;
 
@@ -9,32 +10,33 @@ namespace DotNet7.Controllers
     public class CharacterController : ControllerBase
     {
 
-        private static List<Character> characters = new List<Character> {
-            new Character(),
-            new Character {Id =1, Name = "Sam"}
-    };
+       
+        private readonly ICharacterService _characterService;
+
+        public CharacterController(ICharacterService characterService)
+        {
+            _characterService = characterService;
+        }
 
 
         [HttpGet]
         [Route(("GetAll"))]
         public ActionResult<List<Character>> GetAll()
         {
-            return Ok(characters);
+            return Ok(_characterService.GetAllCharacters());
         }
 
         [HttpGet]
         [Route("{id}")]
         public ActionResult<List<Character>> GetSingle(int id)
         {
-            return Ok(characters.FirstOrDefault(c => c.Id == id));
+            return Ok(_characterService.GetCharacterById(id));
         }
 
         [HttpPost]
         public ActionResult<List<Character>> AddCharacter(Character newCharacter)
         {
-
-            characters.Add(newCharacter);
-            return Ok(characters);
+            return Ok(_characterService.AddCharacter(newCharacter));
         }
     }
 }
---
+++ DotNet7/Services/CharacterService/ICharacterService.cs
@@ -0,0 +1,11 @@
+﻿using DotNet7.Models;
+
+namespace DotNet7.Services.CharacterService
+{
+    public interface ICharacterService
+    {
+        List<Character> GetAllCharacters();
+        Character GetCharacterById(int id);
+        List<Character> AddCharacter(Character newCharacter);
+    }
+}
