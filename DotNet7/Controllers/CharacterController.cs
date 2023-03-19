fa8167a14df23181807de59ea998506781ed220d
Esan Alorizi <62729091+EsanOrizi@users.noreply.github.com>
2023-03-19 21:03:13 +0000

    Add project files.

---
+++ DotNet7/Controllers/CharacterController.cs
@@ -0,0 +1,40 @@
+﻿using DotNet7.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Writers;
+
+namespace DotNet7.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CharacterController : ControllerBase
+    {
+
+        private static List<Character> characters = new List<Character> {
+            new Character(),
+            new Character {Id =1, Name = "Sam"}
+    };
+
+
+        [HttpGet]
+        [Route(("GetAll"))]
+        public ActionResult<List<Character>> GetAll()
+        {
+            return Ok(characters);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public ActionResult<List<Character>> GetSingle(int id)
+        {
+            return Ok(characters.FirstOrDefault(c => c.Id == id));
+        }
+
+        [HttpPost]
+        public ActionResult<List<Character>> AddCharacter(Character newCharacter)
+        {
+
+            characters.Add(newCharacter);
+            return Ok(characters);
+        }
+    }
+}
