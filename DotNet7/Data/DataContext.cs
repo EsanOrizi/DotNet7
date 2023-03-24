8f655a29d543464ca12195fbff7886001e6435fd
Esan Alorizi <62729091+EsanOrizi@users.noreply.github.com>
2023-03-24 18:31:12 +0000

    DataContext added and First Migration

---
+++ DotNet7/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using DotNet7.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNet7.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+                        
+        }
+
+        public DbSet<Character> Characters => Set<Character>();
+    }
+}
