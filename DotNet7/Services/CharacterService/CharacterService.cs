65294eef3f8d571a97e614613812bb128c87042a
Esan Alorizi <62729091+EsanOrizi@users.noreply.github.com>
2023-03-29 13:20:49 +0100

    Delete and Get by Id for logged in user

--- DotNet7/Services/CharacterService/CharacterService.cs
+++ DotNet7/Services/CharacterService/CharacterService.cs
@@ -46,9 +46,10 @@ namespace DotNet7.Services.CharacterService
             try
             {
 
-                var character = await _context.Characters.FirstOrDefaultAsync(c => c.Id == id);
+                var character = await _context.Characters
+                    .FirstOrDefaultAsync(c => c.Id == id && c.User!.Id == GetUserId());
 
-                if (character == null)
+                if (character is null)
                 {
                     throw new Exception($"Character with Id '{id}' not found ");
                 }
@@ -57,7 +58,9 @@ namespace DotNet7.Services.CharacterService
 
                 await _context.SaveChangesAsync();
 
-                serviceResponse.Data = await _context.Characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToListAsync();
+                serviceResponse.Data = await _context.Characters
+                    .Where(c => c.User!.Id == GetUserId())
+                    .Select(c => _mapper.Map<GetCharacterDto>(c)).ToListAsync();
             }
             catch (Exception ex)
             {
@@ -81,7 +84,8 @@ namespace DotNet7.Services.CharacterService
         public async Task<ServiceResponse<GetCharacterDto>> GetCharacterById(int id)
         {
             var serviceResponse = new ServiceResponse<GetCharacterDto>();
-            var dbCharacter = await _context.Characters.FirstOrDefaultAsync(c => c.Id == id);
+            var dbCharacter = await _context.Characters
+                .FirstOrDefaultAsync(c => c.Id == id && c.User!.Id == GetUserId());
             serviceResponse.Data = _mapper.Map<GetCharacterDto>(dbCharacter);
             return serviceResponse;
         }
